e086b13251a6d5a210b469c1e55ffb8aed9afb01
Risshi25 <risshirajsen@gmail.com>
2024-07-01 02:24:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import GameMenu from './components/GameMenu';
+import history from './history.js'
+import socket from './socketConfig.js';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(()=> {
+    socket.on('test',msg=>{
+    console.log(msg);
+    })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Routes>
+        <Route exact path="/" element={<GameMenu />} />
+      </Routes>
+    </Router>
   );
 }
 

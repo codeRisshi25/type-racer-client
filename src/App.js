4e0048e9da4b2fbf78ce956d1d9122205a497ebc
Risshi25 <risshirajsen@gmail.com>
2024-07-01 20:06:23 +0530

    Browser Router to index.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
+  useNavigate,
 } from "react-router-dom";
 import GameMenu from "./components/GameMenu";
 import CreateGame from "./components/CreateGame.js";
@@ -10,6 +11,7 @@ import socket from "./socketConfig.js";
 import { useEffect, useState } from "react";
 
 function App() {
+  let navigate = useNavigate();
   const [gameState, setGameState] = useState({
     _id: "",
     isOpen: false,
@@ -27,16 +29,14 @@ function App() {
   }, []);
   useEffect(() => {
     if (gameState._id !== "") {
-      history.push(`/game/${gameState._id}`);
+      navigate(`/game/${gameState._id}`);
     }
   }, [gameState._id]);
   return (
-    <Router history={history}>
       <Routes>
         <Route exact path="/" element={<GameMenu/>} />
         <Route path="/game/create" element={<CreateGame/>}/>
       </Routes>
-    </Router>
   );
 }
 

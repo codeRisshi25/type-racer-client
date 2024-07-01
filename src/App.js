666bf8ca9563ffcab92be46eeb85dd8ffb3c6263
Risshi25 <risshirajsen@gmail.com>
2024-07-01 19:50:19 +0530

    history vs navigate

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import GameMenu from './components/GameMenu';
-import history from './history.js'
-import socket from './socketConfig.js';
-import { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import GameMenu from "./components/GameMenu";
+import CreateGame from "./components/CreateGame.js";
+import history from "./history.js";
+import socket from "./socketConfig.js";
+import { useEffect, useState } from "react";
 
 function App() {
-  useEffect(()=> {
-    socket.on('test',msg=>{
-    console.log(msg);
-    })
-  })
+  const [gameState, setGameState] = useState({
+    _id: "",
+    isOpen: false,
+    players: [],
+    words: [],
+  });
+  useEffect(() => {
+    socket.on("updateGame", (game) => {
+      console.log(game);
+      setGameState(game);
+    });
+    return () => {
+      socket.removeAllListeners();
+    }
+  }, []);
+  useEffect(() => {
+    if (gameState._id !== "") {
+      history.push(`/game/${gameState._id}`);
+    }
+  }, [gameState._id]);
   return (
     <Router history={history}>
       <Routes>
-        <Route exact path="/" element={<GameMenu />} />
+        <Route exact path="/" element={<GameMenu/>} />
+        <Route path="/game/create" element={<CreateGame/>}/>
       </Routes>
     </Router>
   );
--- src/components/GameMenu.js
+++ src/components/GameMenu.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const GameMenu = props => {
-    let navigate = useNavigate();
-    return(
-        <div className="text-center">
-            <h1>Welcome to Type Racer</h1>
-            <button type="button" onClick={() => navigate('/game/create')}
-                                    className="btn btn-primary btn-lg mr-3">Create Game</button>
-            <button type="button" onClick={() => navigate('/game/join')} // Assuming you meant to navigate to '/game/join' for the second button
-                                    className="btn btn-primary btn-lg mr-3">Join Game</button>
-        </div>
-    )
-}
+const GameMenu = (props) => {
+  let navigate = useNavigate();
+  return (
+    <div className="text-center">
+      <h1>Welcome to Type Racer</h1>
+      <button
+        type="button"
+        onClick={() => navigate("/game/create")}
+        className="btn btn-primary btn-lg mr-3"
+      >
+        Create Game
+      </button>
+      <button
+        type="button"
+        onClick={() => navigate("/game/join")} // Assuming you meant to navigate to '/game/join' for the second button
+        className="btn btn-primary btn-lg mr-3"
+      >
+        Join Game
+      </button>
+    </div>
+  );
+};
 
 export default GameMenu;
